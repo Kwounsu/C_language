f412087ff747be8c49ce6a3c6538a62f9b52dd72
Kwounsu Lee <sb00lgs@gmail.com>
2021-06-27 19:55:15 +0900

    vector, auto

--- 약수.c
+++ 약수.c
@@ -9,9 +9,7 @@ using namespace std;
 // 정수 n의 약수를 모두 구해서 반환한다.
 vector<int> divisor(int n) {
     vector<int> v;
-    int i, j, cnt=0;
-    
-    for (i = 1; i * i <= n; i++) {
+    for (int i = 1; i * i <= n; i++) {
         if (n % i == 0) {
             v.push_back(i);
             if (n / i != i)
